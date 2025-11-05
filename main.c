f46b2302d11a14c67d4f04c66b8a99ba099c6fd3
agalotaibi <hopes.gmo@gmail.com>
2025-11-05 07:43:47 +0000

    update the main file to solve task 2

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include "shell.h"
 
+/**
+* main - Entry point for the simple shell program.
+* Return: Always 0 on success.
+*/
 int main(void)
 {
 char *buffer = NULL;
 size_t bufsize = 0;
 ssize_t chars_read;
 pid_t child_pid;
-char **argv;
+char *argv[2];
 
 while (1)
 {
@@ -21,38 +25,33 @@ printf("\n");
 break;
 }
 
-if (buffer[chars_read - 1] == '\n')
 buffer[chars_read - 1] = '\0';
 
-if (buffer[0] == '\0' || strspn(buffer, " \t") == strlen(buffer))
+if (buffer[0] == '\0')
 continue;
-
-argv = parse_input(buffer);
-if (argv == NULL || argv[0] == NULL)
-{
-free(argv);
-continue;
-}
 
 child_pid = fork();
 if (child_pid == -1)
 {
-perror("fork");
+perror("Error:");
 break;
 }
 
 if (child_pid == 0)
 {
-execve(argv[0], argv, environ);
-perror(argv[0]);
+argv[0] = buffer;
+argv[1] = NULL;
+
+if (execve(argv[0], argv, environ) == -1)
+{
+perror("./shell");
 exit(EXIT_FAILURE);
+}
 }
 else
 {
 wait(NULL);
 }
-
-free(argv);
 }
 
 free(buffer);
