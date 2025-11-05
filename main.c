1ea69fe333b60fb2973a00e7fb6d5da7f0fa5ba2
agalotaibi <hopes.gmo@gmail.com>
2025-11-05 07:51:45 +0000

    update the main file to solve task 2

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ break;
 }
 
 buffer[chars_read - 1] = '\0';
+buffer = trim_spaces(buffer);
 
 if (buffer[0] == '\0')
 continue;
@@ -56,4 +57,28 @@ wait(NULL);
 
 free(buffer);
 return (0);
+}
+
+
+/**
+* trim_spaces - Removes leading and trailing spaces from a string
+* @str: String to trim
+* Return: Pointer to the trimmed string (same pointer)
+*/
+char *trim_spaces(char *str)
+{
+int start = 0, end;
+
+if (str == NULL)
+return (NULL);
+
+while (str[start] == ' ' || str[start] == '\t')
+start++;
+
+end = strlen(str) - 1;
+while (end >= start && (str[end] == ' ' || str[end] == '\t'))
+end--;
+
+str[end + 1] = '\0';
+return (str + start);
 }
