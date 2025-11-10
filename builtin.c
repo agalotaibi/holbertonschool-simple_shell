bfefe00fb7eca07c1911a5c0a2dc4ec48e473263
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-10 14:45:36 +0000

    check for betty in builtin.c file

--- builtin.c
+++ builtin.c
@@ -18,3 +18,18 @@ exit(0);
 
 return (0);
 }
+/**
+ * print_env - print env
+ *
+ * Return: void
+ **/
+void print_env(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
