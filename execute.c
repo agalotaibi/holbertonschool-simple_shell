618e7b7255a8c3dcdab141599265a332277babd7
Lamyaa439 <lamyaaalghaihab@gmail.com>
2025-11-10 15:31:01 +0000

    check betty for execute.c file

--- execute.c
+++ execute.c
@@ -1,24 +1,19 @@
 #include "shell.h"
-
 /**
 * execute_command - Executes a command
 * @line: The command line to execute
-*
 * Return: Exit status of command
-*/
+**/
 int execute_command(char *line)
 {
 pid_t pid;
-int status;
+int status, argc;
 char *argv[MAX_ARGS], *command_path = NULL;
-int argc;
 static int cmd_count = 1;
 if (line == NULL || line[0] == '\0')
 return (0);
 argc = parse_command(line, argv);
-if (argc == 0)
-return (0);
-if (check_builtin(argv))
+if (argc == 0 || check_builtin(argv))
 return (0);
 command_path = get_location(argv[0]);
 if (command_path == NULL)
@@ -34,7 +29,7 @@ perror("fork");
 free(command_path);
 return (-1);
 }
-else if (pid == 0)
+if (pid == 0)
 {
 if (execve(command_path, argv, environ) == -1)
 {
@@ -43,13 +38,10 @@ free(command_path);
 exit(127);
 }
 }
-else
-{
 waitpid(pid, &status, 0);
 free(command_path);
 cmd_count++;
 if (WIFEXITED(status))
 return (WEXITSTATUS(status));
-}
 return (0);
 }
